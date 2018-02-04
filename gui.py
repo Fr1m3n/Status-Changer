dda825764c47b0a7022a5efd2ecfc8269c3dc768
Fr1m3n <34315303+Fr1m3n@users.noreply.github.com>
2018-02-04 19:11:33 +0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -5,6 +5,6 @@ import sys
 session = vk.AuthSession('6354496', sys.argv[1], sys.argv[2], scope = 'status')
 vk_api = vk.API(session)
 while 1 == 1:
-    now = time.localtime()
+    now = time.localtime(time.time() + 3600 * 3)
     vk_api.status.set(text = "Сегодня: " + str(now[2] // 10) + str(now[2] % 10) + '.' + str(now[1] // 10) + str(now[1] % 10) + '.' + str(now[0]) + ". Время: " + str(now[3] // 10) + str(now[3] % 10) + ':' + str(now[4] // 10) + str(now[4] % 10) + '.')
     time.sleep(60 - now[5])
