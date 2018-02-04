d0c82d91b1f7ee57ef77ab1275f8bc842901c378
Fr1m3n <r.devyatilov@yandex.ru>
2018-02-04 18:45:23 +0300

    try

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=E:\VK_API\env_heroky"
+set "VIRTUAL_ENV=E:\VK_API\env_heroky_git"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
@@ -9,7 +9,7 @@ if defined _OLD_VIRTUAL_PROMPT (
     )
     set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
 )
-set "PROMPT=(env_heroky) %PROMPT%"
+set "PROMPT=(env_heroky_git) %PROMPT%"
 
 REM Don't use () to avoid problems with them in %PATH%
 if defined _OLD_VIRTUAL_PYTHONHOME goto ENDIFVHOME
